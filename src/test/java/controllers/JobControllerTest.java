20944330cee205c317886bcfe1c6b5f9cf9796a6
Jonathan Foucher <jonathan-foucher@outlook.fr>
2024-07-07 14:12:18 +0200

    add tests

---
+++ src/test/java/controllers/JobControllerTest.java
@@ -0,0 +1,69 @@
+package controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jonathanfoucher.redistopicexample.controllers.JobController;
+import com.jonathanfoucher.redistopicexample.data.JobDto;
+import com.jonathanfoucher.redistopicexample.services.JobQueuePublisher;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(JobController.class)
+@SpringJUnitConfig(JobController.class)
+class JobControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private JobQueuePublisher jobQueuePublisher;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final String START_JOB_PATH = "/v1/jobs/start";
+    private static final Long JOB_ID = 15L;
+    private static final String JOB_NAME = "some job name";
+
+    @Test
+    void startJob() throws Exception {
+        // GIVEN
+        JobDto job = initJobDto();
+
+        // WHEN / THEN
+        mockMvc.perform(post(START_JOB_PATH)
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(job))
+                )
+                .andExpect(status().isOk());
+
+        ArgumentCaptor<JobDto> capturedJob = ArgumentCaptor.forClass(JobDto.class);
+        verify(jobQueuePublisher, times(1))
+                .publish(capturedJob.capture());
+
+        assertEquals(1, capturedJob.getAllValues().size());
+        checkJob(capturedJob.getValue());
+    }
+
+    private JobDto initJobDto() {
+        JobDto job = new JobDto();
+        job.setId(JOB_ID);
+        job.setName(JOB_NAME);
+        return job;
+    }
+
+    private void checkJob(JobDto job) {
+        assertNotNull(job);
+        assertEquals(JOB_ID, job.getId());
+        assertEquals(JOB_NAME, job.getName());
+    }
+}
---
+++ src/test/java/services/JobQueueSubscriberTest.java
@@ -0,0 +1,82 @@
+package services;
+
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jonathanfoucher.redistopicexample.data.JobDto;
+import com.jonathanfoucher.redistopicexample.services.JobQueueSubscriber;
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import static ch.qos.logback.classic.Level.INFO;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringJUnitConfig(JobQueueSubscriber.class)
+class JobQueueSubscriberTest {
+    @Autowired
+    private JobQueueSubscriber jobQueueSubscriber;
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final String TOPIC_NAME = "job_queue";
+    private static final Long JOB_ID = 15L;
+    private static final String JOB_NAME = "some job name";
+
+    @Test
+    void onMessageReceived() {
+        // GIVEN
+        Logger log = (Logger) LoggerFactory.getLogger(JobQueueSubscriber.class);
+        ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+        listAppender.start();
+        log.addAppender(listAppender);
+
+        JobDto job = initJobDto();
+        Message message = initMessage(job);
+
+        // WHEN
+        jobQueueSubscriber.onMessage(message, TOPIC_NAME.getBytes(StandardCharsets.UTF_8));
+
+        // THEN
+        List<ILoggingEvent> logs = listAppender.list;
+        assertEquals(2, logs.size());
+
+        assertEquals(INFO, logs.get(0).getLevel());
+        assertEquals("starting to process job " + job, logs.get(0).getFormattedMessage());
+
+        assertEquals(INFO, logs.get(1).getLevel());
+        assertEquals("successfully processed job " + job, logs.get(1).getFormattedMessage());
+    }
+
+    private JobDto initJobDto() {
+        JobDto job = new JobDto();
+        job.setId(JOB_ID);
+        job.setName(JOB_NAME);
+        return job;
+    }
+
+    private Message initMessage(JobDto job) {
+        return new Message() {
+            @Override
+            public byte[] getBody() {
+                try {
+                    return objectMapper.writeValueAsBytes(job);
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            @Override
+            public byte[] getChannel() {
+                return TOPIC_NAME.getBytes(StandardCharsets.UTF_8);
+            }
+        };
+    }
+}
