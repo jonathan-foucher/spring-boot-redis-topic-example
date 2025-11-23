d91f62c5aa88c08462988fff16ac709fd1b3d51b
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2025-11-23 14:26:13 +0100

    chore: upgrade dependencies and java versions

--- src/main/java/com/jonathanfoucher/redistopicexample/RedisTopicExampleApplication.java
+++ src/main/java/com/jonathanfoucher/redistopicexample/RedisTopicExampleApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RedisTopicExampleApplication {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(RedisTopicExampleApplication.class, args);
     }
 }
